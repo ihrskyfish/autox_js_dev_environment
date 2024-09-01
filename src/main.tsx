650f550b647506f968a5bdf7457a36ac2bad8b00
ihrskyfish <ihr17867950748@126.com>
2024-09-01 10:13:34 +0800

    就算再main.tsx中使用settimeout函数  也报错 不能在ui线程执行阻塞操作，

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { area } from "./myUtils";
 import debounce from "lodash-es/debounce";
 
-require('./main.js');
-
+import {js_main}  from "./main2"
+import { set } from "lodash-es";
 
 // find that  is is  always enter a autoxjs  page
-toast("Hello, World! i am here      ==========================-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+
+
+// js_main(); 
 
 
 
+setTimeout(() => {
+   js_main(); 
+}, 1); 
