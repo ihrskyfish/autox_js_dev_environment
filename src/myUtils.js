8a08e3c17001a499f975d2b0bd22c362949d6c07
ihrskyfish <ihr17867950748@126.com>
2024-09-01 16:12:56 +0800

    utils中添加了一个随机定时的setTimeout_random

--- src/myUtils.js
+++ src/myUtils.js
@@ -3,4 +3,11 @@ var area = function (r) {
     return PI * r * r;
 };
 var circumference = function (r) { return 2 * PI * r; };
-export { area, circumference };
+function setTimeout_random(callback, minMilliseconds=0, maxMilliseconds=8 * 60 * 60 * 1000) {
+    const randomDelay = Math.floor(Math.random() * (maxMilliseconds - minMilliseconds + 1)) + minMilliseconds;
+    // 使用 setTimeout 在随机时间间隔后执行回调函数
+    setTimeout(callback, randomDelay);
+}
+
+
+export { area, circumference ,setTimeout_random};
